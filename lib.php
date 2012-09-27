bd79fb8cf304e939a9ceb28824b3f624617a3a90
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-09-28 00:37:46 +0200

    added getImageInfo() function

--- lib.php
+++ lib.php
@@ -29,3 +29,21 @@ function hex2color($hexStr, $returnAsString = false, $seperator = ',') {
 	
 	return $returnAsString ? implode($seperator, $rgbArray) : $rgbArray;
 }
+
+function getImageInfo(&$image) {
+	$imageInfo = array(
+		'width' => imagesx($image),
+		'height' => imagesy($image),
+		'colorGrid' => array()
+	);
+	
+	for ($x = 0; $x < $imageInfo['width']; $x++) {
+		$imageInfo['colorGrid'][$x] = array();
+		
+		for ($y = 0; $y < $imageInfo['height']; $y++) {
+			$imageInfo['colorGrid'][$x][$y] = imagecolorsforindex($image, imagecolorat($image, $x, $y));
+		}
+	}
+	
+	return $imageInfo;
+}
