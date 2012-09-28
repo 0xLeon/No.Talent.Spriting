d25b73e5d0d11edac71874191fe917614fb330b2
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-09-28 12:41:15 +0200

    added colorToHex() function and re-introduced getColorGrid() function

--- lib.php
+++ lib.php
@@ -1,6 +1,12 @@
 <?php
 if (strtolower(php_sapi_name()) != 'cli') die('Script has to be invoked from cli');
 
+define('PX_ARGB', 1);
+define('PX_RGBA', 2);
+define('PX_ABGR', 4);
+define('PX_BGRA', 8);
+define('PX_ARRAY', 16);
+
 function ownDecHex($number) {
 	if ($number < 0x10) return '0'.dechex($number);
 	else return dechex($number);
@@ -30,9 +36,38 @@ function hex2color($hexStr, $returnAsString = false, $seperator = ',') {
 	return $returnAsString ? implode($seperator, $rgbArray) : $rgbArray;
 }
 
+function colorToHex($color, $order = PX_ARGB) {
+	switch ($order) {
+		case PX_BGRA:
+			return ($color['alpha'] + ($color['red'] << 8) + ($color['green'] << 16) + ($color['blue'] << 24));
+		case PX_ABGR:
+			return ($color['red'] + ($color['green'] << 8) + ($color['blue'] << 16) + ($color['alpha'] << 24));
+		case PX_RGBA:
+			return ($color['alpha'] + ($color['blue'] << 8) + ($color['green'] << 16) + ($color['red'] << 24));
+		case PX_ARGB:
+		default:
+			return ($color['blue'] + ($color['green'] << 8) + ($color['red'] << 16) + ($color['alpha'] << 24));
+	}
+}
+
 function getImageInfo(&$image) {
 	return array(
 		'width' => imagesx($image),
 		'height' => imagesy($image)
 	);
+}
+
+function getColorGrid(&$image, $pixelType = PX_ARRAY) {
+	$imageInfo = getImageInfo($image);
+	$colorGrid = array();
+	
+	for ($y = 0; $y < $imageInfo['height']; $y++) {
+		$colorGrid[$y] = array();
+		
+		for ($x = 0; $x < $imageInfo['width']; $x++) {
+			$colorGrid[$y][$x] = (($pixelType === PX_ARRAY) ? imagecolorsforindex($image, imagecolorat($image, $x, $y)) : colorToHex(imagecolorsforindex($image, imagecolorat($image, $x, $y)), $pixelType));
+		}
+	}
+	
+	return $colorGrid;
 }
